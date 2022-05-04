f2694a22a25921a827f66353705d76504849e1ad
VTamNichi <91141563+VTamNichi@users.noreply.github.com>
2022-05-05 01:15:18 +0700

    Delete AmateurFootballLeague directory

--- AmateurFootballLeague/AmateurFootballLeague/Models/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AmateurFootballLeague.Models
-{
-    public class Role
-    {
-        public int ID { get; set; }
-        public string RoleName { get; set; }
-        public List<User> Users { get; set; }
-    }
-}
