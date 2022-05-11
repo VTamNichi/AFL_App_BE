cc6b64e0fa3175d5b679e6af3dd95285b1abf734
tuananh <tuananh200055@gmail.com>
2022-05-11 14:21:51 +0700

    UpdateCors

--- AmateurFootballLeague/Program.cs
+++ AmateurFootballLeague/Program.cs
@@ -18,8 +18,19 @@ using Newtonsoft.Json.Converters;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          //                          policy.WithOrigins("http://example.com",
+                          //                                             "http://www.contoso.com");
+                          policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                      });
+});
 // Add services to the container.
 
 builder.Services.AddDbContext<AmateurFootballLeagueContext>(options =>
@@ -140,6 +151,8 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+
 
 app.UseAuthentication();
 
