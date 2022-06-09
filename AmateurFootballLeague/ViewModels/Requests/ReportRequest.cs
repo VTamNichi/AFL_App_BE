f79852c4de7f768c2f504a406f03ece3c6e90e7b
Nguyen Van Tam <vantam1417@gmail.com>
2022-06-10 03:46:52 +0700

    add report and notification controller

---
+++ AmateurFootballLeague/ViewModels/Requests/ReportRequest.cs
@@ -0,0 +1,32 @@
+﻿namespace AmateurFootballLeague.ViewModels.Requests
+{
+    public enum ReportFieldEnum
+    {
+        Id,
+        Reason,
+        DateReport,
+        UserId,
+        CommentId,
+        TeamId,
+        TournamentId
+    }
+
+    public class ReportCM
+    {
+        public string? Reason { get; set; }
+        public int UserId { get; set; }
+        public int? CommentId { get; set; }
+        public int? TeamId { get; set; }
+        public int? TournamentId { get; set; }
+    }
+
+    public class ReportUM
+    {
+        public int Id { get; set; }
+        public string? Reason { get; set; }
+        public int? UserId { get; set; }
+        public int? CommentId { get; set; }
+        public int? TeamId { get; set; }
+        public int? TournamentId { get; set; }
+    }
+}
