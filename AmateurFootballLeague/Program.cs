fb3cd055307cb6ef937b06471dc840b370c1f942
VTamNichi <91141563+VTamNichi@users.noreply.github.com>
2022-07-02 02:05:40 +0700

    Update Program.cs

--- AmateurFootballLeague/Program.cs
+++ AmateurFootballLeague/Program.cs
@@ -72,7 +72,7 @@ builder.Services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
 builder.Services.AddSingleton<ChangeStatusTournamentService>();
 builder.Services.AddSingleton(new JobSchedule(
     jobType: typeof(ChangeStatusTournamentService),
-    cronExpression: "0 0/5 19 * * ?"));
+    cronExpression: "0 0/5 17 * * ?"));
 
 builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
 
