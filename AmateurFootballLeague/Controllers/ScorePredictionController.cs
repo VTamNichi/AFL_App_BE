47fd095a2b6dc794ac65ccc0f90403ed1829c73d
tuananh <tuananh200055@gmail.com>
2022-07-22 00:23:50 +0700

    ai la nguoi du doan dung nhat vay ha van tam chinh la tuan anh

--- AmateurFootballLeague/Controllers/ScorePredictionController.cs
+++ AmateurFootballLeague/Controllers/ScorePredictionController.cs
@@ -19,18 +19,20 @@ namespace AmateurFootballLeague.Controllers
         private readonly IMatchService _matchService;
         private readonly ITournamentService _tournamentService;
         private readonly ITeamService _teamService1;
+        private readonly ITeamInMatchService _teamInMatch;
         private readonly ITeamInTournamentService _teamInTournamentService;
         private readonly IPlayerInTeamService _playerInTeamService;
         private readonly IPlayerInTournamentService _playerInTournamentService;
         public ScorePredictionController (IScorePredictionService scorePrediction , IMapper mapper, IMatchService matchService,
             ITournamentService tournamentService, IUserService userService, ITeamService teamService1, ITeamInTournamentService teamInTournamentService,
-            IPlayerInTournamentService playerInTournamentService, IPlayerInTeamService playerInTeamService)
+            IPlayerInTournamentService playerInTournamentService, IPlayerInTeamService playerInTeamService , ITeamInMatchService teamInMatch)
         {
             _scorePrediction = scorePrediction; 
             _mapper = mapper;
             _matchService = matchService;
             _tournamentService = tournamentService;
             _teamService1 = teamService1;
+            _teamInMatch = teamInMatch;
             _teamInTournamentService= teamInTournamentService;
             _playerInTeamService = playerInTeamService;
             _playerInTournamentService = playerInTournamentService;
@@ -204,6 +206,24 @@ namespace AmateurFootballLeague.Controllers
             }
         }
 
+        [HttpGet("truePredict")]
+        public ActionResult<ScorePredictionVM> GetTrueScorePrediction(int matchId)
+        {
+            try
+            {
+                ScorePrediction scorePredict = _scorePrediction.GetList().Where(s => s.MatchId == matchId && s.Status == "true").FirstOrDefault();
+                if (scorePredict == null)
+                {
+                    return NotFound("Không có dự đoán trong trận này ");
+                }
+                return Ok(_mapper.Map<ScorePredictionVM>(scorePredict));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
         [HttpPut]
         public async Task<ActionResult> UpdateScorePrediction(ScorePredictionUM model)
         {
@@ -252,15 +272,235 @@ namespace AmateurFootballLeague.Controllers
 
         [HttpPut]
         [Route("Status")]
-        public async Task<ActionResult> ChangeStatusScorePrediction(int id , string status)
+        public async Task<ActionResult> ChangeStatusScorePrediction(int matchId)
         {
             try
             {
-                ScorePrediction scorePrediction = await _scorePrediction.GetByIdAsync(id);
+                IQueryable<TeamInMatch> result =  _teamInMatch.GetList().Where(s => s.MatchId == matchId);
+                var listResult = result.ToList();
+                IQueryable<ScorePrediction> scorePrediction = _scorePrediction.GetList().Where(s => s.MatchId == matchId);
+                if(scorePrediction == null)
+                {
+                    return NotFound("Không có dự đoán cho trận đấu này");
+                }
+                ScorePrediction closetScore = new ScorePrediction();
                 if (scorePrediction != null)
                 {
-                    scorePrediction.Status = status;
-                    bool isUpdated = await _scorePrediction.UpdateAsync(scorePrediction);
+                    closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == 2 && s.TeamBscore == 2).FirstOrDefault();
+                    if(closetScore == null)
+                    {
+                        closetScore =  scorePrediction.Where(s=> s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == 2 && s.TeamBscore == 2).FirstOrDefault();
+                    }
+
+                    if(closetScore == null)
+                    {
+    
+                        List<ScorePrediction> listScoreOneSide = new List<ScorePrediction>();
+                        if(listResult[0].TeamScore > listResult[1].TeamScore)
+                        {
+                            int scoreB = (int)listResult[1].TeamScore;
+                            listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == listResult[0].TeamScore && s.TeamBscore < listResult[0].TeamScore).ToList();
+                            if (listScoreOneSide == null || listScoreOneSide.Count()==0)
+                            {
+                                 scoreB = (int)listResult[0].TeamScore;
+                                listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == listResult[1].TeamScore && s.TeamBscore < listResult[1].TeamScore).ToList();
+                            }
+
+                            if (listScoreOneSide.Count() > 0)
+                            {
+                                int minScore = (int)listScoreOneSide[0].TeamBscore;
+                                int min = Math.Abs(scoreB -minScore);
+                                foreach (ScorePrediction s in listScoreOneSide)
+                                {
+                                    var checkMin = Math.Abs(scoreB - (int)s.TeamBscore);
+                                    if(checkMin < min)
+                                    {
+                                        min = checkMin;
+                                        minScore = (int)s.TeamBscore;
+                                    }
+                                }
+
+                                closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == listResult[0].TeamScore && s.TeamBscore == minScore).FirstOrDefault();
+                                if (closetScore == null)
+                                {
+                                    closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == listResult[1].TeamScore && s.TeamBscore == minScore).FirstOrDefault();
+                                }
+                            }
+                            if(closetScore == null)
+                            {
+                                listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id &&  s.TeamBscore < s.TeamAscore).ToList();
+                                int scoreA = (int)listResult[0].TeamScore;
+                                 scoreB = (int)listResult[1].TeamScore;
+                                if (listScoreOneSide == null || listScoreOneSide.Count() == 0)
+                                {
+                                     scoreA = (int)listResult[1].TeamScore;
+                                     scoreB = (int)listResult[0].TeamScore;
+                                    listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamBscore < s.TeamAscore).ToList();
+                                }
+                                if (listScoreOneSide.Count() > 0)
+                                {
+                                    int minScoreA = (int)listScoreOneSide[0].TeamAscore;
+                                    int minScoreB = (int)listScoreOneSide[0].TeamBscore;
+                                    int min = Math.Abs(scoreA - minScoreA) + Math.Abs(scoreB - minScoreB);
+                                    int dif = Math.Abs(minScoreA - minScoreB);
+                                    foreach (ScorePrediction s in listScoreOneSide)
+                                    {
+                                        var checkMin = Math.Abs(scoreA - (int)s.TeamAscore) + Math.Abs(scoreB - (int)s.TeamBscore);
+                                        var checkDif = s.TeamAscore - s.TeamBscore;
+                                        if (checkMin < min)
+                                        {
+                                           min = checkMin;
+                                            minScoreA = (int)s.TeamAscore;
+                                            minScoreB = (int)s.TeamBscore;
+                                        }
+                                    }
+
+                                    closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == minScoreA && s.TeamBscore == minScoreB).FirstOrDefault();
+                                    if (closetScore == null)
+                                    {
+                                        closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == minScoreA && s.TeamBscore == minScoreB).FirstOrDefault();
+                                    }
+                                }
+                            }
+
+                        }
+                        if (listResult[0].TeamScore < listResult[1].TeamScore)
+                        {
+                            int scoreA = (int)listResult[0].TeamScore;
+                            listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore < listResult[1].TeamScore && s.TeamBscore == listResult[1].TeamScore).ToList();
+                            if (listScoreOneSide == null || listScoreOneSide.Count() == 0)
+                            {
+                                 scoreA = (int)listResult[1].TeamScore;
+                                listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore < listResult[0].TeamScore && s.TeamBscore == listResult[0].TeamScore).ToList();
+                            }
+                            if (listScoreOneSide.Count() > 0)
+                            {
+                                int minScore = (int)listScoreOneSide[0].TeamAscore;
+                                 scoreA = (int)listResult[0].TeamScore;
+                                int min = Math.Abs(scoreA - minScore);
+                                foreach (ScorePrediction s in listScoreOneSide)
+                                {
+                                    var checkMin = Math.Abs(scoreA - (int)s.TeamAscore);
+                                    if (checkMin < min)
+                                    {
+                                        min = checkMin;
+                                        minScore = (int)s.TeamAscore;
+                                    }
+                                }
+
+                                closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == minScore && s.TeamBscore == listResult[1].TeamScore).FirstOrDefault();
+                                if (closetScore == null)
+                                {
+                                    closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == minScore && s.TeamBscore == listResult[0].TeamScore).FirstOrDefault();
+                                }
+                            }
+
+                            if (closetScore == null)
+                            {
+                                listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamBscore > s.TeamAscore).ToList();
+                                 scoreA = (int)listResult[0].TeamScore;
+                                int scoreB = (int)listResult[1].TeamScore;
+                                if (listScoreOneSide == null || listScoreOneSide.Count() == 0)
+                                {
+                                    scoreA = (int)listResult[1].TeamScore;
+                                    scoreB = (int)listResult[0].TeamScore;
+                                    listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamBscore > s.TeamAscore).ToList();
+                                }
+                                if (listScoreOneSide.Count() > 0)
+                                {
+                                    int minScoreA = (int)listScoreOneSide[0].TeamAscore;
+                                    int minScoreB = (int)listScoreOneSide[0].TeamBscore;
+                                    int min = Math.Abs(scoreA - minScoreA) + Math.Abs(scoreB - minScoreB);
+                                    int dif = Math.Abs(minScoreA - minScoreB);
+                                    foreach (ScorePrediction s in listScoreOneSide)
+                                    {
+                                        var checkMin = Math.Abs(scoreA - (int)s.TeamAscore) + Math.Abs(scoreB - (int)s.TeamBscore);
+                                        var checkDif = s.TeamAscore - s.TeamBscore;
+                                        if (checkMin < min)
+                                        {
+                                            min = checkMin;
+                                            minScoreA = (int)s.TeamAscore;
+                                            minScoreB = (int)s.TeamBscore;
+                                        }
+                                    }
+
+                                    closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == minScoreA && s.TeamBscore == minScoreB).FirstOrDefault();
+                                    if (closetScore == null)
+                                    {
+                                        closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == minScoreA && s.TeamBscore == minScoreB).FirstOrDefault();
+                                    }
+                                }
+                            }
+
+                        }
+                        if (listResult[0].TeamScore == listResult[1].TeamScore)
+                        {
+                            ScorePrediction drawUp = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == listResult[0].TeamScore+1 && s.TeamBscore == listResult[1].TeamScore + 1).FirstOrDefault();
+                            if (drawUp == null)
+                            {
+                                drawUp = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == listResult[1].TeamScore + 1 && s.TeamBscore == listResult[0].TeamScore + 1).FirstOrDefault();
+                            }
+                            ScorePrediction drawDown = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == listResult[0].TeamScore - 1 && s.TeamBscore == listResult[1].TeamScore - 1).FirstOrDefault();
+                            if (drawDown == null)
+                            {
+                                drawDown = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == listResult[1].TeamScore - 1 && s.TeamBscore == listResult[0].TeamScore - 1).FirstOrDefault();
+                            }
+                            if(drawUp != null && drawDown != null)
+                            {
+                                if (drawUp.Id < drawDown.Id)
+                                {
+                                    closetScore = drawUp;
+                                }
+                                else
+                                {
+                                    closetScore = drawDown;
+                                }
+                            }
+
+                            if (drawUp != null && drawDown == null)
+                            {
+                                    closetScore = drawUp;             
+                            }
+
+                            if (drawUp == null && drawDown != null)
+                            {
+                                closetScore = drawDown;
+                            }
+
+                            if (closetScore == null)
+                            {
+                                listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == s.TeamBscore).ToList();
+                                if (listScoreOneSide == null || listScoreOneSide.Count() == 0)
+                                {
+                                    listScoreOneSide = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore ==s.TeamBscore).ToList();
+                                }
+                                int scoreB = (int)listResult[1].TeamScore;
+
+                                if (listScoreOneSide.Count() > 0)
+                                {
+                                    int minScore = (int)listScoreOneSide[0].TeamBscore;
+                                    int min = Math.Abs(scoreB - minScore);
+                                    foreach (ScorePrediction s in listScoreOneSide)
+                                    {
+                                        var checkMin = Math.Abs(scoreB - (int)s.TeamBscore);
+                                        if (checkMin < min)
+                                        {
+                                            min = checkMin;
+                                            minScore = (int)s.TeamBscore;
+                                        }
+                                    }
+
+                                    closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[0].Id && s.TeamInMatchBid == listResult[1].Id && s.TeamAscore == minScore && s.TeamBscore == minScore).FirstOrDefault();
+                                    if (closetScore == null)
+                                    {
+                                        closetScore = scorePrediction.Where(s => s.TeamInMatchAid == listResult[1].Id && s.TeamInMatchBid == listResult[0].Id && s.TeamAscore == minScore && s.TeamBscore == minScore).FirstOrDefault();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    closetScore.Status = "true";
+                    bool isUpdated = await _scorePrediction.UpdateAsync(closetScore);
                     if (isUpdated)
                     {
                         return Ok(new
@@ -270,7 +510,7 @@ namespace AmateurFootballLeague.Controllers
                     }
                     return BadRequest("Thay đổi trạng thái dự đoán tỷ số thất bại");
                 }
-                return NotFound("Không tìm thấy dự đoán tỷ số với id là " + id);
+                return NoContent();
             }
             catch
             {
