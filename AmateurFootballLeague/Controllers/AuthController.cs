284dbada2cd6214b454f2318a7b65eb3959e5936
Nguyen Van Tam <vantam1417@gmail.com>
2022-08-21 22:45:50 +0700

    k

--- AmateurFootballLeague/Controllers/AuthController.cs
+++ AmateurFootballLeague/Controllers/AuthController.cs
@@ -242,6 +242,7 @@ namespace AmateurFootballLeague.Controllers
                     "<img src='https://afl-app-fe.vercel.app/assets/img/homepage/logo.png' alt='' class='logo' style='display: inline-flex; color: #fff; width: 60px;' width='60'/>" +
                     "</header>" +
                     "<div class='form_mail' style='font-family: Arial, Helvetica, sans-serif; padding: 50px; background-color: #f4f7f6; max-width: 590px; margin: 0px auto; line-height: 24px;'>" +
+                    
                     "<div class='title' style='display: block; font-size: 1.5em; margin-block-start: 0.83em; margin-block-end: 0.83em; margin-inline-start: 0px; margin-inline-end: 0px; font-weight: bold;'>Gửi mã xác nhận</div>" +
                     "<p class='hello' style='display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px;'>Xin chào <a href='mailto:" + email + "'>" + email + "</a>,</p>" +
                     "<p class='content' style='display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px;'>" +
@@ -250,8 +251,9 @@ namespace AmateurFootballLeague.Controllers
                     "<p class='content' style='display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px;'>" +
                     "Mã xác thực của bạn là: " + code.ToString() +
                     "</p>" +
+                    
                     "<a href='https://afl-app-fe.vercel.app/' style='background-color: #00afab; border-radius: 50px; padding: 10px 20px; display: inline-flex; color: #fff; font-weight: bold; text-decoration: none; text-transform: uppercase; font-size: 14px; margin-top: 10px;'>Trang chủ AFL</a>" +
-                    "<p class='thank' style='display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px;'>Xin cảm ơn bạn,</p>" +
+                    "<p class='thank' style='display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px;'>Xin cảm ơn,</p>" +
                     "<p class='thanks' style='display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px; font-weight: bold;'>Amateur Football League</p>" +
                     "</div>" +
                     "<footer style='padding: 30px 50px; max-width: 590px; margin: 0px auto; font-size: 13px; background: #ddd; color: #767676; font-family: Arial, Helvetica, sans-serif; line-height: 24px;'>" +
